d9b9481c6ede5a76f84087a13950d6c7e160721a
yoon <dudwls143@gmail.com>
2022-05-19 00:21:05 +0900

    feat: API 문서화 시작

--- src/main/java/dev/konkuk/home/domain/account/api/AccountController.java
+++ src/main/java/dev/konkuk/home/domain/account/api/AccountController.java
@@ -2,9 +2,13 @@ package dev.konkuk.home.domain.account.api;
 
 import dev.konkuk.home.domain.account.dto.AccountDto;
 import dev.konkuk.home.domain.account.service.AccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "소셜 로그인 Controller")
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 @RestController
@@ -12,6 +16,8 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @ApiOperation(value = "서버 로그인 API")
+//    @ApiImplicitParam(name = "tagIds", value = "검색할 태그 ID를 담은 리스트", dataType = "list")
     @PostMapping("/login")
     public AccountDto login(@RequestBody AccountDto accountDto) {
         if (!accountService.isRegisteredAccount(accountDto.getEmail())) {
--- src/main/java/dev/konkuk/home/domain/account/dto/AccountDto.java
+++ src/main/java/dev/konkuk/home/domain/account/dto/AccountDto.java
@@ -1,18 +1,25 @@
 package dev.konkuk.home.domain.account.dto;
 
 import dev.konkuk.home.domain.account.entity.Account;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ToString
+@ApiModel(value = "로그인 시 서버에 보낼 정보")
 @Getter @Setter
 @Builder
 public class AccountDto {
 
+    @ApiModelProperty(value = "회원 이름", example = "윤영진")
     private String name;
+
+    @ApiModelProperty(value = "회원 이메일", example = "dudwls143@gmail.com")
     private String email;
+
+    @ApiModelProperty(value = "회원 사진")
     private String imageThumbnail;
 
     public Account toEntity() {
