d58b8ab5dc4b1c906fbf6b0f12966f11b9c09c60
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-05-27 19:12:28 +0900

    Refactor: 매물 상세보기 요청 시 이메일 받도록 추가
    - 관심 매물 여부를 반환하기 위해 요청 시 이메일을 받도록 수정

--- src/main/java/dev/konkuk/home/domain/favorite/api/FavoriteController.java
+++ src/main/java/dev/konkuk/home/domain/favorite/api/FavoriteController.java
@@ -1,7 +1,11 @@
 package dev.konkuk.home.domain.favorite.api;
 
+import dev.konkuk.home.domain.account.entity.Account;
+import dev.konkuk.home.domain.account.service.AccountService;
 import dev.konkuk.home.domain.favorite.dto.FavoriteDto;
 import dev.konkuk.home.domain.favorite.service.FavoriteService;
+import dev.konkuk.home.domain.property.entity.Property;
+import dev.konkuk.home.domain.property.service.PropertyService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -18,6 +22,8 @@ import java.util.List;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+    private final AccountService accountService;
+    private final PropertyService propertyService;
 
     @ApiOperation(value = "유저의 관심 매물 목록 조회 API")
     @ApiImplicitParam(name = "email", value = "유저 이메일")
@@ -30,7 +36,9 @@ public class FavoriteController {
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping
     public void createFavorite(@RequestBody FavoriteDto.Request favoriteRequestDto) {
-        favoriteService.createFavorite(favoriteRequestDto.getEmail(), favoriteRequestDto.getItemId());
+        Account account = accountService.findByEmail(favoriteRequestDto.getEmail());
+        Property property = propertyService.getById(favoriteRequestDto.getItemId());
+        favoriteService.createFavorite(account, property);
     }
 
 }
